1f065a820fcdd6b96daef2041cd158629812dd4f
Bharat Patidar <pritamgam786@gmail.com>
2022-11-15 17:55:10 +0530

    setup cursor and back feature

--- src/Components/TypingBox.js
+++ src/Components/TypingBox.js
@@ -2,20 +2,99 @@ import React, { createRef, useEffect, useRef, useState } from 'react'
 
 const TypingBox = ({words}) => {
     const inputTextRef = useRef(null);
+
      const[currCharIndex,setCurrCharIndex] = useState(0);
      const[currWordIndex,setCurrWordIndex] = useState(0);
+     const[countDown,setCountDown]= useState(15);
+     const[testStart,setTestStart]= useState(false);
+     const[testOver,setTestOver]= useState(false);
 
     const wordSpanRef = Array(words.length).fill(0).map(i=>createRef(null));
-    console.log(wordSpanRef);
+     
+    const startTimer = ()=>{
+        const intervalId = setInterval(() => {
+            setCountDown(pre=>{
+                if(pre===1){
+                    clearInterval(intervalId);
+                    setCountDown(0);
+                    setTestOver(true)
+                }
+                else{
+                    return pre -1;
+                }
+            });
+        },1000);
+    }
     const handlekey =(e)=>{
+        if(!testStart){
+            startTimer();
+            setTestStart(true);
+        }
+       
         let allChildrenSpan = wordSpanRef[currWordIndex].current.childNodes;
-            console.log(e.key)
+         if(e.keyCode===32){
+            if(allChildrenSpan.length <= currCharIndex){
+                allChildrenSpan[currCharIndex-1].classList.remove('right');
+            }
+            else{
+                allChildrenSpan[currCharIndex].className = allChildrenSpan[currCharIndex].className.replace('current','');
+            }
+            
+            wordSpanRef[currWordIndex+1].current.childNodes[0].className = 'char current';
+
+            setCurrWordIndex(setCurrWordIndex=>setCurrWordIndex+1);
+            setCurrCharIndex(0);  
+            return; 
+         }
+         
+         if(e.keyCode===8){
+
+            if(currCharIndex!==0){
+                if(currCharIndex===allChildrenSpan.length){
+                    
+                    if(allChildrenSpan[currCharIndex-1].className.includes('extra')){
+                        allChildrenSpan[currCharIndex-1].remove();
+                        allChildrenSpan[currCharIndex-2].className +='right';
+                    }else{
+                        allChildrenSpan[currCharIndex-1].className ='char current';
+                    }
+
+                   setCurrCharIndex(currCharIndex-1);
+                   return;
+                 }
+
+                allChildrenSpan[currCharIndex].className = 'char';
+                allChildrenSpan[currCharIndex-1].className = 'char current';
+                setCurrCharIndex(currCharIndex-1);
+            }
+            return;
+         }
+         
+         if(currCharIndex===allChildrenSpan.length){
+          let newSpan = document.createElement('span');
+          newSpan.innerText = e.key;
+          newSpan.className = 'char inCorrect right extra';
+          allChildrenSpan[currCharIndex-1].className = allChildrenSpan[currCharIndex-1].className.replace('right','');
+
+          wordSpanRef[currWordIndex].current.append(newSpan);
+          setCurrCharIndex(currCharIndex+1);
+            return;
+         }
+
          if(e.key===allChildrenSpan[currCharIndex].innerText){
             allChildrenSpan[currCharIndex].className = 'char correct';
          }
          else{
             allChildrenSpan[currCharIndex].className = 'char inCorrect';
          }
+
+         if(currCharIndex+1 === allChildrenSpan.length){
+            allChildrenSpan[currCharIndex].className +=' right';
+         }
+         else{ 
+            allChildrenSpan[currCharIndex+1].className = 'char current';
+         }
+         
          setCurrCharIndex(currCharIndex=>currCharIndex+1);
     }
     const focusInput=()=>{
@@ -23,20 +102,24 @@ const TypingBox = ({words}) => {
     }
     useEffect(()=>{
        focusInput();
+       wordSpanRef[0].current.childNodes[0].className= 'char current';
     },[])
   return (
     <div>
-    <div className='type-box' onClick={focusInput}>
+     <h1>{countDown}</h1>
+     {testOver? (<h1>Test Over</h1>):(<div className='type-box' onClick={focusInput}>
         <div className='words'>
               {words.map((word,index)=>(
-                <span className="word" ref={wordSpanRef[index]}>
+                <span className="word" ref={wordSpanRef[index]} key={index}>
                     {word.split("").map((char,idx)=>(
-                        <span>{char}</span>
+                        <span className='char' key={`char${idx}`}>{char}</span>
                     ))}
                 </span>
               ))}
         </div>
-    </div>
+      </div>
+    )}
+    
     <input type='text' className='hidden-Input' ref={inputTextRef} onKeyDown={(e)=>handlekey(e)}/>
     </div>
   )
--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -13,8 +13,8 @@ export const GlobalStyles = createGlobalStyle`
     padding :0;
     margin: 0;
     transition: all 0.25s linear;
-
   }
+
   .canvas{
     display : grid;
     grid-auto-flow: row;
@@ -25,6 +25,7 @@ export const GlobalStyles = createGlobalStyle`
     width: 100vw;
     align-items:center;
   }
+
   .type-box{
        display:block;
        max-width:1000px;
@@ -34,6 +35,7 @@ export const GlobalStyles = createGlobalStyle`
        margin-right:auto;
        overflow: hidden;
   }
+
   .words{
        font-size: 25px;
        display: flex;
@@ -41,17 +43,50 @@ export const GlobalStyles = createGlobalStyle`
        align-content:center;
        width: 100%;  
   }
+
   .word{
     margin:5px;
     padding-right:2px;
   }
+
   .hidden-Input{
     opacity:0;
   }
+
   .correct{
     color: green;
   }
+
   .inCorrect{
     color:red;
+  }
+  
+  .current{
+    border-left: 1px solid;
+    animation : blinking 2s infinite;
+    animation-timing-function: ease;
+
+    @keyframes blinking{
+      0% {border-left-color:#fff;};
+      25% {border-left-color:black;};
+      50% {border-left-color:#fff;};
+      75% {border-left-color:black;};
+      100% {border-left-color:#fff;};
+    }
+  }
+
+  .right{
+    border-right: 1px solid;
+    animation : blinkingRight 2s infinite;
+    animation-timing-function: ease;
+
+    @keyframes blinkingRight{
+      0% {border-right-color:#fff;};
+      25% {border-right-color:black;};
+      50% {border-right-color:#fff;};
+      75% {border-right-color:black;};
+      100% {border-right-color:#fff;};
+    }
+
   }
 `;
